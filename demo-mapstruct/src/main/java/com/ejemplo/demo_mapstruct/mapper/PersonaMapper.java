3f248962659c0ba4492ec11daa0262c4f28ec7a9
nagicome03 <nayeligiselle.0206@gmail.com>
2026-07-10 09:14:51 +0200

    live coding

--- demo-mapstruct/src/main/java/com/ejemplo/demo_mapstruct/mapper/PersonaMapper.java
+++ demo-mapstruct/src/main/java/com/ejemplo/demo_mapstruct/mapper/PersonaMapper.java
@@ -1,41 +1,18 @@
 package com.ejemplo.demo_mapstruct.mapper;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.ejemplo.demo_mapstruct.dto.PersonaRequestDTO;
 import com.ejemplo.demo_mapstruct.dto.PersonaResponseDTO;
 import com.ejemplo.demo_mapstruct.entity.Persona;
 
-@Component
-public class PersonaMapper {
-public Persona toEntity(PersonaRequestDTO dto) {
+@Mapper(componentModel = "spring")
+public interface PersonaMapper {
 
-        Persona persona = new Persona();
+    @Mapping(source = "direction", target = "direccion")
+    Persona toEntity(PersonaRequestDTO dto);
 
-        persona.setName(dto.getName());
-        persona.setSurname(dto.getSurname());
-        persona.setEmail(dto.getEmail());
-        persona.setPhone(dto.getPhone());
-        persona.setDireccion(dto.getDirection());
-        persona.setBirthdate(dto.getBirthdate());
-        persona.setActive(dto.getActive());
+    PersonaResponseDTO toDTO(Persona persona);
 
-        return persona;
-    }
-
-    public PersonaResponseDTO toDTO(Persona persona) {
-
-        PersonaResponseDTO dto = new PersonaResponseDTO();
-
-        dto.setId(persona.getId());
-        dto.setName(persona.getName());
-        dto.setSurname(persona.getSurname());
-        dto.setEmail(persona.getEmail());
-        dto.setPhone(persona.getPhone());
-        dto.setDirection(persona.getDireccion());
-        dto.setBirthdate(persona.getBirthdate());
-        dto.setActive(persona.getActive());
-
-        return dto;
-    }
 }
